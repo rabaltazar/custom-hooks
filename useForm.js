e8ccb0af0dd510fbe2f044c16d3c595a120889bb
Raul Baltazar <rabaltazar@coca-cola.com>
2021-03-03 23:42:50 -0600

    first commit

---
+++ useForm.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+
+export const useForm = (initialState = {}) => {
+    const [formState, setFormState] = useState(initialState);
+
+    const reset = () => {
+        setFormState(initialState)
+    }
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+    return [formState, handleInputChange, reset]
+}
